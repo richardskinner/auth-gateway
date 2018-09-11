bea363eca1eb32543ebd24720b267fe92ea2a240
Richard Skinner <richard.skinner@simplestream.com>
2018-09-11 14:33:37 +0100

    Reverted Transformer

--- src/AuthGateway/Auth/Transformers/Auth0.php
+++ src/AuthGateway/Auth/Transformers/Auth0.php
@@ -11,8 +11,8 @@ class Auth0 implements TransformerInterface
         return [
             'id' => $data['user_id'],
             'email' => $data['email'],
-            'first_name' => $data['first_name'] ? $data['first_name'] : null,
-            'last_name' => $data['last_name'] ? $data['last_name'] : null,
+            'first_name' => $data['user_metadata']['first_name'] ? $data['user_metadata']['first_name'] : null,
+            'last_name' => $data['user_metadata']['last_name'] ? $data['user_metadata']['last_name'] : null,
             'account_code' => $data['user_metadata']['recurly']['account_code'] ? $data['user_metadata']['recurly']['account_code'] : null,
             'created_at' => isset($data['created_at']) ? new \DateTime($data['created_at']) : null,
             'updated_at' => isset($data['updated_at']) ? new \DateTime($data['updated_at']) : null, //Field doesn't exist but needs to consistent with other transformers
